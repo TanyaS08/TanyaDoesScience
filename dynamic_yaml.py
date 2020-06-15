cc029c5f18b5f4cc61fd22260a3e5d69b7a9ec70
Tanya Strydom <tanya.strydom@icloud.com>
2020-06-15 16:13:30 +0100

    added comments to dynamic_yaml.py; added .css for potential Gallery page

--- dynamic_yaml.py
+++ dynamic_yaml.py
@@ -3,16 +3,21 @@
 import yaml
 import os
 
+#set wd
 os.chdir('/Users/tanyastrydom/Documents/TanyaDoesScience/posts')
 
+#get file names matching specific search string
 import glob
 for name in glob.glob('2020*'):
     print (name)
-
+    
+#define structure of .yml file
 dict_file = [{'listing' : 'posts' : [name]},
 {'countries' : ['Pakistan', 'USA', 'India', 'China', 'Germany', 'France', 'Spain']}]
 
+#convert to YAML language
 yaml.dump(dict_file)
 
-with open(r'/Users/tanyastrydom/Documents/TanyaDoesScience/store_file.yaml', 'w') as file:
+#conver to .yml file and save to wd
+with open(r'/Users/tanyastrydom/Documents/TanyaDoesScience/dynamic_yaml.yml', 'w') as file:
     documents = yaml.dump(dict_file, file)
